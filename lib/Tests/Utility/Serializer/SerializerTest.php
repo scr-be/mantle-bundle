e143d455d656dc0341d97423a6c702cac4b3a256
Rob Frawley 2nd <rfrawley@scribenet.com>
2015-03-22 12:07:45 -0400

    forgot to include some new tests

---
+++ lib/Tests/Utility/Serializer/SerializerTest.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * This file is part of the Scribe Mantle Bundle.
+ *
+ * (c) Scribe Inc. <source@scribe.software>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Scribe\Tests\Utility\Serializer;
+
+use PHPUnit_Framework_TestCase;
+use Scribe\Utility\Serializer\Serializer;
+
+class SerializerTest extends PHPUnit_Framework_TestCase
+{
+    public function testThrowsExceptionOnInstantiation()
+    {
+        $this->setExpectedException(
+            'Scribe\Exception\RuntimeException',
+            'Cannot instantiate static class Scribe\Utility\Serializer\Serializer'
+        );
+
+        new Serializer();
+    }
+
+    public function testSerializerIgbinary()
+    {
+        $expectedUnserialized = [1, 'something', ['2, 3', 4]];
+        $expectedSerialized   = igbinary_serialize($expectedUnserialized);
+
+        $serialized   = Serializer::sleep($expectedUnserialized, Serializer::SERIALIZE_METHOD_IGBINARY);
+        $unserialized = Serializer::wake($serialized, Serializer::SERIALIZE_METHOD_IGBINARY);
+        $this->assertEquals($expectedSerialized, $serialized);
+        $this->assertEquals($expectedUnserialized, $unserialized);
+    }
+
+    public function testSerializerJson()
+    {
+        $expectedUnserialized = [1, 'something', ['2, 3', 4]];
+        $expectedSerialized   = json_encode($expectedUnserialized);
+
+        $serialized   = Serializer::sleep($expectedUnserialized, Serializer::SERIALIZE_METHOD_JSON);
+        $unserialized = Serializer::wake($serialized, Serializer::SERIALIZE_METHOD_JSON);
+        $this->assertEquals($expectedSerialized, $serialized);
+        $this->assertEquals($expectedUnserialized, $unserialized);
+    }
+
+    public function testSerializerNative()
+    {
+        $expectedUnserialized = [1, 'something', ['2, 3', 4]];
+        $expectedSerialized   = serialize($expectedUnserialized);
+
+        $serialized   = Serializer::sleep($expectedUnserialized, Serializer::SERIALIZE_METHOD_NATIVE);
+        $unserialized = Serializer::wake($serialized, Serializer::SERIALIZE_METHOD_NATIVE);
+        $this->assertEquals($expectedSerialized, $serialized);
+        $this->assertEquals($expectedUnserialized, $unserialized);
+    }
+}
+
+/* EOF */
