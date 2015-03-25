db00903f0935e10c1f0f3a0d88d21760ea525330
dcorrigan <dcorrigan@scribenet.com>
2015-03-25 13:58:43 -0400

    added dummy author entity for testing purposes

---
+++ src/Tests/Fixtures/app/Entity/Author.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This file is part of the Scribe Mantle Bundle.
+ *
+ * (c) Scribe Inc. <source@scribe.software>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Scribe\MantleBundle\Tests\Fixtures\app\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Scribe\MantleBundle\Model\AuthorInterface;
+
+class Author implements AuthorInterface 
+{
+    /**
+     * @var integer 
+     */
+    private $id = null;
+
+    /**
+     * @var ArrayCollection 
+     */
+    private $revisions;
+
+    /**
+     * @var ArrayCollection 
+     */
+    private $nodes;
+
+    /**
+     * perform any entity setup
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function getNodeRevisions() {}
+    public function setNodeRevisions(ArrayCollection $nodeRevisions) {}
+    public function getNodes() {}
+    public function setNodes(ArrayCollection $nodes) {}
+}
