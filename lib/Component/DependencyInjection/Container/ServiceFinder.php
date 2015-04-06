c88107d0f3989e60eb470cf60cea6d6df04bb25f
dcorrigan <dcorrigan@scribenet.com>
2015-04-06 13:42:17 -0400

    added service finder utility service

--- lib/Component/DependencyInjection/Container/ServiceFinder.php
+++ lib/Component/DependencyInjection/Container/ServiceFinder.php
@@ -48,6 +48,6 @@ class ServiceFinder
             return $this->container->get($service);
         }
 
-        throw new RuntimeException('Placeholder exception because service not found!');
+        throw new RuntimeException(sprintf('Service %s not found.', $service));
     }
 }
